63f4746066df11a1948951c7fa27b3a4ba9793b3
Sinchana-M-S <sinchanams932@gmail.com>
2025-09-04 19:55:34 +0530

    design updated

--- Myntra_react_clone/src/components/Footer.jsx
+++ Myntra_react_clone/src/components/Footer.jsx
@@ -1,49 +1,59 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="footer_container">
-        <div className="footer_column">
-          <h3>ONLINE SHOPPING</h3>
+    <div class="footer_container">
+      <div class="footer_column">
+        <h3>ONLINE SHOPPING</h3>
+        <a href="#">Men</a>
+        <a href="#">Women</a>
+        <a href="#">Kids</a>
+        <a href="#">Home</a>
+        <a href="#">Beauty</a>
+        <a href="#">Genz</a>
+        <a href="#">Gift Cards</a>
+        <a href="#">Myntra Insider</a>
+      </div>
 
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">Kids</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Beauty</a>
-          <a href="#">Gift Card</a>
-          <a href="#">Myntra Insider</a>
-        </div>
+      <div class="footer_column">
+        <h3>CUSTOMER POLICIES</h3>
+        <a href="#">Contact Us</a>
+        <a href="#">FAQ</a>
+        <a href="#">T&C</a>
+        <a href="#">Terms Of Use</a>
+        <a href="#">Track Orders</a>
+        <a href="#">Shipping</a>
+        <a href="#">Cancellations</a>
+        <a href="#">Returns</a>
+        <a href="#">Privacy policy</a>
+        <a href="#">Grievance Redressal</a>
+      </div>
 
-        <div className="footer_column">
-          <h3>ONLINE SHOPPING</h3>
+      <div class="footer_column">
+        <h3>USEFUL LINKS</h3>
+        <a href="#">Blog</a>
+        <a href="#">Careers</a>
+        <a href="#">Site Map</a>
+        <a href="#">Corporate Information</a>
+        <a href="#">Whitehat</a>
+        <a href="#">Cleartrip</a>
+        <a href="#">Myntra Global</a>
+      </div>
 
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">Kids</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Beauty</a>
-          <a href="#">Gift Card</a>
-          <a href="#">Myntra Insider</a>
-        </div>
-
-        <div className="footer_column">
-          <h3>ONLINE SHOPPING</h3>
-
-          <a href="#">Men</a>
-          <a href="#">Women</a>
-          <a href="#">Kids</a>
-          <a href="#">Home & Living</a>
-          <a href="#">Beauty</a>
-          <a href="#">Gift Card</a>
-          <a href="#">Myntra Insider</a>
+      <div class="footer_column">
+        <h3>KEEP IN TOUCH</h3>
+        <div class="icon_container">
+          <div class="icon">
+            <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          </div>
         </div>
       </div>
-      <hr />
-
-      <div className="copyright">
-        © 2023 www.myntra.com. All rights reserved.
-      </div>
-    </footer>
+    </div>
+    <hr />
+    <div class="copyright">© 2025 www.myntra.com. All rights reserved.</div>
+  </footer>
   );
 };
 
